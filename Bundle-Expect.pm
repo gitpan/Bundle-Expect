c17cb27615d97528a2f67a6c2636861f82a7cd53
Roland Giersig <RGiersig@cpan.org>
2000-11-21 18:01:38 +0000

    Import of RGIERSIG/Bundle-Expect-1.09 from CPAN.
    gitpan-cpan-distribution: Bundle-Expect
    gitpan-cpan-version:      1.09
    gitpan-cpan-path:         RGIERSIG/Bundle-Expect-1.09.tar.gz
    gitpan-cpan-author:       RGIERSIG
    gitpan-cpan-maturity:     released

--- Bundle-Expect.pm
+++ Bundle-Expect.pm
@@ -1,29 +1,29 @@
 package Bundle::Expect;
 
-$VERSION = '1.08';
+$VERSION = '1.09';
 
 1;
 
 __END__
 
 =head1 NAME
- 
+
 Bundle::Expect - Everything that Expect.pm needs
- 
+
 =head1 SYNOPSIS
- 
+
 C<perl -MCPAN -e 'install Bundle::Expect'>
- 
+
 =head1 CONTENTS
- 
-Expect 1.08
- 
-IO::Stty 0.02
- 
+
+Expect
+
+IO::Stty
+
 IO::Tty
- 
+
 IO::Pty
- 
+
 =head1 DESCRIPTION
 
 Everything that Expect.pm needs. What can I say more?
